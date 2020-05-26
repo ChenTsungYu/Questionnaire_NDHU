b3a64bede2eb2b33f1b0119f3085058833b6cb57
TSUNG YU CHEN <46296838+ChenTsungYu@users.noreply.github.com>
2020-05-26 10:11:22 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -7,7 +7,7 @@ elif [ "${instruction}" = "ui" ]; then
     echo "pack ui"
     pyuic5 Questionnaire.ui > qui.py
 elif [ "${instruction}" = "win" ]; then
-    echo "pack exe for windows, plz comfirm that docker is running and in the folder named 'src'! "
+    echo "pack exe for windows, plz confirm that docker is running and your're in the folder named 'src'! "
     docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows
 else
     echo "plz imput correct instruction like: pack, ui, win"
