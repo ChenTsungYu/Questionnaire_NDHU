6d5ab05e7493e29d89bc0bd09dc3d08e73613251
chentsungyu <z3337059@gmail.com>
2020-05-26 10:24:38 +0800

    modify pack.sh

--- pack.sh
+++ pack.sh
@@ -2,26 +2,15 @@
 read -p "Please input your instruction: " instruction
 case $instruction in
     "exe") 
-        echo "pack exe";;
-    "ui") 
-        echo "pack ui";;
+        echo "pack exe"
+        pyinstaller -F ./qt_crawler.py;;
      "ui") 
-        echo "pack ui";;
+        echo "pack ui"
+        pyuic5 Questionnaire.ui > qui.py;;
      "win") 
-        echo "pack exe for windows, plz confirm that docker is running and your're in the folder named 'src'! ";;
+        echo "pack exe for windows, plz confirm that docker is running and your're in the folder named 'src'! "
+        docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows;;
     *) 
         echo "plz input correct instruction like: pack, ui, win"
         exit 1
 esac
-# if [ "${instruction}" = "exe" ]; then
-#     echo "pack exe"
-#     pyinstaller -F ./qt_crawler.py
-# elif [ "${instruction}" = "ui" ]; then
-#     echo "pack ui"
-#     pyuic5 Questionnaire.ui > qui.py
-# elif [ "${instruction}" = "win" ]; then
-#     echo "pack exe for windows, plz confirm that docker is running and your're in the folder named 'src'! "
-#     docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows
-# else
-#     echo "plz input correct instruction like: pack, ui, win"
-# fi
